7396fa616a5d95c95bde8980ba994b4d9952de73
ayush kanoongo <ayush.kanoongo@sell.do>
2022-01-24 17:42:02 +0530

    [UPDATE] - Update leaderboard sql query

--- app/controllers/leader_boards_controller.rb
+++ app/controllers/leader_boards_controller.rb
@@ -1,7 +1,7 @@
 class LeaderBoardsController < ApplicationController
 
   def index
-    member_ids = Member.joins(:participants).group('members.id').having("count(participants.id)>=10")
+    member_ids = Member.joins(:participants).where(participants: {status: 'win'}).group('members.id').having("count(participants.id)>=10")
     @members = Member.where(id: member_ids).joins(:participants)
       .select("members.id, members.first_name, members.last_name, avg(participants.score) as average_score, count(participants.id) as number_of_matches")
       .group("members.id")
